a2487947dcd1e20ffc8e23495c98a1308b396c86
incid <incid@DESKTOP-LFA5FLK>
2024-12-19 19:46:30 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void cerceve(int x1, int y1, int x2, int y2)
+        {
+            for (int i = y1; i <= y2; i++)
+            {
+                Console.SetCursorPosition(i, x1); Console.WriteLine("*");
+                Console.SetCursorPosition(i, x2); Console.WriteLine("*");
+
+            }
+
+            for (int i = x1 + 1; i <= x2 - 1; i++)
+            {
+                Console.SetCursorPosition(y1, i); Console.WriteLine("*");
+                Console.SetCursorPosition(y2, i); Console.WriteLine("*");
+
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+
+            cerceve(4, 10,7, 60);
+
+
+            {
+            }
+        }
+    }
+}
